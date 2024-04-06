6829cf2181480e83f1d77ecd74b31b3b3a7456c3
Linn-Htet123 <tlh.linnhtet@gmail.com>
2024-04-06 22:55:29 +0630

    refactor: [linnhtet] refactor the MenuChildItem

--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -1,10 +1,9 @@
-
 const HistoryPage = () => {
   return (
     <div className=" absolute flex p-2 w-[80%] text-[#d4d4d4] right-0 h-screen justify-center items-center ">
       History
     </div>
-  )
-}
+  );
+};
 
-export default HistoryPage
+export default HistoryPage;
