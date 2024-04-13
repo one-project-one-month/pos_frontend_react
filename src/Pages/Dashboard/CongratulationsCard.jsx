0f0d4a67819a2cbe97e717327c23f1866138f689
lizzy-km <oxygenisme.02@gmail.com>
2024-04-13 22:46:11 +0630

    Hee Hee

--- src/Pages/Dashboard/CongratulationsCard.jsx
+++ src/Pages/Dashboard/CongratulationsCard.jsx
@@ -10,21 +10,21 @@ const CongratulationsCard = ({totalProfit}) => {
 
     }} 
     
-    className="flex absolute left-0 w-[68%]    transition-all flex-col items-end  ">
+    className="flex absolute left-0 w-[68%] transition-l flex-col items-end  ">
       <div className="card w-full  rounded-lg shadow-lg bg-[#312d4b] text-[#d4d4d4] ">
         <div className="flex w-full justify-between ">
           <div className="w-[60%] px-4  py-4">
             <h4 className="card-title text-lg mb-4 text-truncate">Congratulations John! </h4>
             <p className="mb-0 w-full py-4 opacity-70 font-thin tracking-wider ">You have done {totalProfit.toFixed(2)}%  more sales today.<br /> Check your new raising badge in your profile.</p>
           </div>
-          <div style={{
-            display: totalProfit < 0 ? 'none' : 'flex'
-          }} className=" w-[40%]  relative  justify-end items-end px-4">
+          <div  className="  w-[40%]  relative  justify-end items-end px-4">
             <img style={{
-            bottom: totalProfit < 0 ? '-100%' : '0%',
+            bottom: totalProfit < 0 ? '-50%' : '0%',
+            zIndex:totalProfit < 0 ? -1 : 1,
+
           }}
               src="/src/Images/illustration-john-2.png"
-              className="card-img-position w-full absolute h-[130%]  object-cover "
+              className="card-img-position flex w-full absolute h-[130%]  object-cover "
               alt="View Profile"
             />
           </div>
