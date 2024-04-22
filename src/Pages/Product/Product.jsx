5106023da91b71b71dcb02dd040f6202069dfc99
Sann-Ei-Phyo <https://github.com/Sann-Ei-Phyo/porfolio-website.git>
2024-04-22 12:42:22 +0630

    product theme update

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -12,6 +12,10 @@ import {
   exportSettingOn,
   pageOn,
   setPageNum,
+  
+} from "../../redux/services/animateSlice";
+import {
+ 
 } from "../../redux/services/animateSlice";
 
 //icons
@@ -25,22 +29,20 @@ const Product = () => {
   const { addCatForm, page, exportSet, pageNum } = useSelector(state => state.animateSlice);
   const dispatch = useDispatch();
 
+  const color = useSelector((state)=> state.animateSlice)
   const pageCount = [7, 10, 20, 50, 70, 100];
   const navigate = useNavigate();
 
-  //fetch data using react useQuery 
-  const { isLoading, isError, data } = useQuery('products', async () => {
-    //simulate API call delay
+  // Fetch data using react-query
+  const { data } = useQuery('products', async () => {
     await new Promise(resolve => setTimeout(resolve, 1000));
     return products;
   });
 
-
   // Dispatch the fetched data directly within useQuery
-  if (!isLoading && data) {
+  if (data) {
     dispatch(setProductList(data));
   }
-  if (isError) return <div>Error fetching data</div>;
 
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage] = useState(5);
@@ -62,15 +64,29 @@ const Product = () => {
 
   return (
 
-    <div className='flex flex-col gap-3 right-10 w-[80%] top-[50px] ml-[20%] h-auto justify-between items-start p-2 rounded-md bg-[#312d4b]'>
-      <div className='flex  justify-between w-full '>
+    <div 
+     style={{
+      backgroundColor:color.bgColor
+  }}
+    className='flex flex-col gap-3 right-10 w-[80%] top-[50px] ml-[20%] h-auto justify-between items-start p-2 rounded-md bg-[#312d4b]'>
+      <div 
+      
+      className='flex  justify-between w-full '>
 
 
-    <div className='flex flex-col gap-3 right-10 w-full top-[50px] h-auto justify-between items-start p-2 rounded-md bg-[#312d4b]'>
+    <div
+    style={{
+        backgroundColor:color.bgColor
+    }}
+     className='flex flex-col gap-3 right-10 w-full top-[50px] h-auto shadow-md justify-between items-start p-2 rounded-md bg-[#312d4b]'>
 
-      <div className="   flex justify-between items-center w-full h-auto p-1 ">
+      <div className="   flex justify-between items-center w-full h-auto p-1  ">
 
-        <div className=' border-2 px-2  border-[#76728e] flex justify-center items-center rounded-md w-[20%] h-[40px]'>
+        <div style={{
+        borderColor:color.cardBgColor
+    }}
+        
+        className=' border-2 px-2  border-[#76728e] flex justify-center items-center rounded-md w-[20%] h-[40px]'>
           <input
             placeholder="Search Category"
             className=" outline-none rounded-md bg-transparent w-full h-full "
@@ -87,6 +103,10 @@ const Product = () => {
               onClick={() => {
                 dispatch(pageOn({ page: !page }));
               }}
+              
+              style={{
+                color:color.textColor
+            }}
               className=" cursor-pointer flex justify-around items-center rounded w-[15%] h-full border border-[#76728e] "
             >
               <p>{pageNum}</p>
@@ -104,6 +124,10 @@ const Product = () => {
                   exportSettingOn({ exportSet: !exportSet })
                 );
               }}
+              style={{
+                color:color.textColor,
+                backgroundColor:color.cardBgColor
+            }}
               className=" flex justify-evenly tracking-wider items-center text-[#6c6d7b] font-medium  bg-[#3f3c57] cursor-pointer w-[30%] h-full rounded "
             >
               <PiExportThin className=" text-lg " />
@@ -126,10 +150,24 @@ const Product = () => {
                   addCatFormOn({ addCatForm: !addCatForm })
                 )
               }
+              style={{
+                color:color.textColor,
+                backgroundColor:color.cardBgColor
+            }}
               className=" flex justify-between px-3 items-center  bg-[#9055fd] cursor-pointer w-[40%] h-full rounded "
             >
-              <RiAddLine className=" text-lg " />
-              <p className=" font-semibold text-[15px] tracking-wide ">
+              <RiAddLine 
+              style={{
+                color:color.textColor,
+                backgroundColor:color.cardBgColor
+            }}
+             className=" text-lg " />
+              <p style={{
+                color:color.textColor,
+                backgroundColor:color.cardBgColor
+            }}
+              
+              className=" font-semibold text-[15px] tracking-wide ">
                 ADD CATEGORY
               </p>
             </div>
@@ -173,7 +211,12 @@ const Product = () => {
             }}
             className="  flex flex-col justify-center items-center gap-3 absolute shadow-lg w-[25%] h-[150px] rounded bg-[#312d4b]  left-[25%] top-[100%] "
           >
-            <div className=" cursor-pointer gap-1 w-full justify-start items-center flex p-2 rounded text-[#6c6d7b] ">
+            <div
+            style={{
+              color:color.textColor,
+              backgroundColor:color.cardBgColor
+          }}
+            className=" cursor-pointer gap-1 w-full justify-start items-center flex p-2 rounded text-[#6c6d7b] ">
               <MdOutlineLocalPrintshop className=" text-lg " />
               <p
                 onClick={PrintTable}
@@ -190,14 +233,30 @@ const Product = () => {
       {/**Table */}
       <div className="flex w-[100%] justify-between items-center">
         <div id="catListTable" className="w-[100%] text-sm text-gray-500">
-          <table className=" w-[100%] text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <thead>
+          <table
+            style={{
+              color:color.textColor,
+              backgroundColor:color.cardBgColor
+          }} 
+          
+
+           className=" w-[100%] text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <thead
+             style={{
+              color:color.textColor,
+              backgroundColor:color.cardBgColor
+          }}
+            >
               <tr>
                 <th scope="col" className="p-4">
                   <div className="flex items-center">
                     <input
                       id="checkbox-all-search"
                       type="checkbox"
+                      style={{
+                        color:color.textColor,
+                        backgroundColor:color.cardBgColor
+                    }}
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
                     <label htmlFor="checkbox-all-search" className="sr-only">
@@ -233,6 +292,10 @@ const Product = () => {
                       <input
                         id="checkbox-table-search-1"
                         type="checkbox"
+                        style={{
+                          color:color.textColor,
+                          backgroundColor:color.cardBgColor
+                      }}
                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                       />
                       <label htmlFor="checkbox-table-search-1" className="sr-only">
