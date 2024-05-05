6826ad2a6a1583c52ffb8fcce1f5b5f99f99ac03
MgKyaw <mgkyaw20112@gmail.com>
2024-05-05 22:46:59 +0700

    feat: Add loading to Preview Invoice

--- src/Pages/Invoice/PreviewInvoice.jsx
+++ src/Pages/Invoice/PreviewInvoice.jsx
@@ -7,43 +7,29 @@ import { useParams, useNavigate } from "react-router-dom";
 const PreviewInvoice = () => {
   const { saleInvoiceId } = useParams();
   const [saleInvoice, setSaleInvoice] = useState();
+  const [loading, setLoading] = useState(true);
 
   const getData = async () => {
     const baseurl = "https://pos-frontend-next-ruby.vercel.app/api/v1/";
     let saleInvoiceEndpoint = `sale-invoices/${saleInvoiceId}`;
     const saleInvoiceUrl = `${baseurl}${saleInvoiceEndpoint}`;
     console.log("test => " + saleInvoiceUrl);
-    await axios
-      .get(saleInvoiceUrl)
-      .then((res) => {
-        console.log(res.data.data.saleInvoice);
-        setSaleInvoice(res.data.data.saleInvoice);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+    try {
+      const res = await axios.get(saleInvoiceUrl);
+      console.log(res.data.data.saleInvoice);
+      setSaleInvoice(res.data.data.saleInvoice);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setLoading(false); // Set loading to false regardless of success or failure
+    }
   };
 
-  useEffect(()=> {
-    if(saleInvoiceId) {
-      const url = `https://pos-frontend-next-ruby.vercel.app/api/v1/sale-invoices/${saleInvoiceId}`
-      const fetchData = async() => {
-        try {
-          const res = await axios.get(url)
-          const {data :{data :{saleInvoice}}} = res
-          console.log(saleInvoice);
-          setInvoicePreview
-        } catch (error) {
-          console.error(error)
-        }
-      }
-      fetchData()
-    }
-  },[saleInvoiceId])
-
   useEffect(() => {
-    getData();
-  }, []);
+    if (saleInvoiceId) {
+      getData();
+    }
+  }, [saleInvoiceId]);
 
   //const color = useSelector((state) => state.animateSlice);
 
@@ -63,95 +49,101 @@ const PreviewInvoice = () => {
   return (
     <div id="invoice-preview" className="bg-transparent top-[70px] overflow-y-hidden bg-[#312d4b] shadow-md absolute w-[80%] right-0 p-10 h-[91vh]">
       <div className="flex flex-col items-center justify-center w-full ">
-        <div
-          className="flex flex-col justify-center w-1/2 p-10 text-black bg-white rounded-md invoice-preview">
-          <div className="flex flex-col items-center">
-            <h2 className="font-bold">ABC Store</h2>
-            <h3>No(12), Main Road, xyz Township, Yangon, 01-112233</h3>
-            <hr className="my-2 border-black border-dashed" />
-            <table className="table-auto w-full">
-              <tbody>
-                <tr>
-                  <td className="w-[20%]">Voucher No:</td>
-                  <td className="w-[30%]">#{saleInvoice && saleInvoice?.voucherNo}</td>
-                  <td className="w-[20%]">Datetime:</td>
-                  <td className="w-[30%]">{saleInvoice && saleInvoice?.dateTime}</td>
-                </tr>
-                <tr>
-                  <td className="w-[20%]">Customer:</td>
-                  <td className="w-[30%]">{saleInvoice && saleInvoice?.customerCode}</td>
-                  <td className="w-[20%]">Staff:</td>
-                  <td className="w-[30%]">{saleInvoice && saleInvoice?.staff?.staffName}</td>
-                </tr>
-                <tr>
-                  <td className="w-[20%]">Payment Type:</td>
-                  <td className="w-[30%]">{saleInvoice && saleInvoice?.paymentType || "Cash"}</td>
-                  <td className="w-[20%]"></td>
-                  <td className="w-[30%]"></td>
-                </tr>
-              </tbody>
-            </table>
+        {loading ? (
+          <div className="flex flex-col justify-center w-1/2 h-[450px] p-10 text-black bg-white rounded-md invoice-preview">
+
           </div>
-          <hr className="my-5 border-black border-dashed" />
-          <table className="table-auto">
-            <thead>
-              <tr>
-                <th className="text-left">Product</th>
-                <th className="text-right">Quantity</th>
-                <th className="text-right">Price</th>
-                <th className="text-right">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              {saleInvoice &&
-               saleInvoice.saleInvoiceDetails.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.product.productName}</td>
-                  <td className="text-right">{item.quantity.toLocaleString()}</td>
-                  <td className="text-right">{item.price.toLocaleString()}</td>
-                  <td className="text-right">{item.amount.toLocaleString()}</td>
-                </tr>
-              ))}
-              <tr className="border-t border-black border-dashed">
-                <td>Total</td>
-                <td className="text-right">{saleInvoice && saleInvoice.saleInvoiceDetails.reduce((acc, item) => acc + (item.quantity || 0), 0).toLocaleString()}</td>
-                <td></td>
-                <td className="text-right">{saleInvoice && saleInvoice.totalAmount.toLocaleString()}</td>
-              </tr>
-              <tr>
-                <td>Discount</td>
-                <td></td>
-                <td></td>
-                <td className="text-right">-{saleInvoice && saleInvoice.discount.toLocaleString() || 0}</td>
-              </tr>
-              <tr>
-                <td>Tax</td>
-                <td></td>
-                <td></td>
-                <td className="text-right">{saleInvoice && saleInvoice.tax?.toLocaleString() || 0}</td>
-              </tr>
-              <tr className="border-t border-black border-dashed">
-                <td>Paid Amount</td>
-                <td></td>
-                <td></td>
-                <td className="text-right font-bold">{saleInvoice && saleInvoice.receiveAmount?.toLocaleString() || 0}</td>
-              </tr>
-              <tr className="border-t border-black border-dashed">
-                <td>Net Amount</td>
-                <td></td>
-                <td></td>
-                <td className="text-right font-bold">{saleInvoice && saleInvoice.paymentAmount?.toLocaleString() || 0}</td>
-              </tr>
-              <tr className="border-y border-black border-dashed">
-                <td>Change</td>
-                <td></td>
-                <td></td>
-                <td className="text-right font-bold">{saleInvoice && saleInvoice.change?.toLocaleString() || 0}</td>
-              </tr>
-            </tbody>
-          </table>
-          <h3 className="text-center mt-3">Thank you so much for buying from us</h3>
-        </div>
+        ) : (
+          <div
+              className="flex flex-col justify-center w-1/2 p-10 text-black bg-white rounded-md invoice-preview">
+              <div className="flex flex-col items-center">
+                <h2 className="font-bold">ABC Store</h2>
+                <h3>No(12), Main Road, xyz Township, Yangon, 01-112233</h3>
+                <hr className="my-2 border-black border-dashed" />
+                <table className="table-auto w-full">
+                  <tbody>
+                    <tr>
+                      <td className="w-[20%]">Voucher No:</td>
+                      <td className="w-[30%]">#{saleInvoice && saleInvoice?.voucherNo}</td>
+                      <td className="w-[20%]">Datetime:</td>
+                      <td className="w-[30%]">{saleInvoice && saleInvoice?.dateTime}</td>
+                    </tr>
+                    <tr>
+                      <td className="w-[20%]">Customer:</td>
+                      <td className="w-[30%]">{saleInvoice && saleInvoice?.customerCode}</td>
+                      <td className="w-[20%]">Staff:</td>
+                      <td className="w-[30%]">{saleInvoice && saleInvoice?.staff?.staffName}</td>
+                    </tr>
+                    <tr>
+                      <td className="w-[20%]">Payment Type:</td>
+                      <td className="w-[30%]">{saleInvoice && saleInvoice?.paymentType || "Cash"}</td>
+                      <td className="w-[20%]"></td>
+                      <td className="w-[30%]"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <hr className="my-5 border-black border-dashed" />
+              <table className="table-auto">
+                <thead>
+                  <tr>
+                    <th className="text-left">Product</th>
+                    <th className="text-right">Quantity</th>
+                    <th className="text-right">Price</th>
+                    <th className="text-right">Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {saleInvoice &&
+                    saleInvoice.saleInvoiceDetails.map((item, index) => (
+                      <tr key={index}>
+                        <td>{item.product.productName}</td>
+                        <td className="text-right">{item.quantity.toLocaleString()}</td>
+                        <td className="text-right">{item.price.toLocaleString()}</td>
+                        <td className="text-right">{item.amount.toLocaleString()}</td>
+                      </tr>
+                    ))}
+                  <tr className="border-t border-black border-dashed">
+                    <td>Total</td>
+                    <td className="text-right">{saleInvoice && saleInvoice.saleInvoiceDetails.reduce((acc, item) => acc + (item.quantity || 0), 0).toLocaleString()}</td>
+                    <td></td>
+                    <td className="text-right">{saleInvoice && saleInvoice.totalAmount.toLocaleString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Discount</td>
+                    <td></td>
+                    <td></td>
+                    <td className="text-right">-{saleInvoice && saleInvoice.discount.toLocaleString() || 0}</td>
+                  </tr>
+                  <tr>
+                    <td>Tax</td>
+                    <td></td>
+                    <td></td>
+                    <td className="text-right">{saleInvoice && saleInvoice.tax?.toLocaleString() || 0}</td>
+                  </tr>
+                  <tr className="border-t border-black border-dashed">
+                    <td>Paid Amount</td>
+                    <td></td>
+                    <td></td>
+                    <td className="text-right font-bold">{saleInvoice && saleInvoice.receiveAmount?.toLocaleString() || 0}</td>
+                  </tr>
+                  <tr className="border-t border-black border-dashed">
+                    <td>Net Amount</td>
+                    <td></td>
+                    <td></td>
+                    <td className="text-right font-bold">{saleInvoice && saleInvoice.paymentAmount?.toLocaleString() || 0}</td>
+                  </tr>
+                  <tr className="border-y border-black border-dashed">
+                    <td>Change</td>
+                    <td></td>
+                    <td></td>
+                    <td className="text-right font-bold">{saleInvoice && saleInvoice.change?.toLocaleString() || 0}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <h3 className="text-center mt-3">Thank you so much for buying from us</h3>
+            </div>
+        )}
         <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded mt-5" onClick={handlePrint}>
           Print
         </button>
