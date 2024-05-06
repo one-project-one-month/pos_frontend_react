002857019373716365e77be16a3ac053df079ec1
Aung <august810.a@gmail.com>
2024-05-06 15:40:37 +0700

    fixed color of input in addNewInvoice

--- src/Pages/Invoice/SearchInput.jsx
+++ src/Pages/Invoice/SearchInput.jsx
@@ -31,10 +31,10 @@ const SearchInput = () => {
                     <input
                     type='text'
                     id='simple-search'
-                    className='bg-white border border-gray-300 text-gray-900 text-sm
-                    rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-3
-                    p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-                    dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    className='bg-gray-700 border border-gray-700 text-sm
+                    rounded-lg focus:ring-blue-500 block w-full ps-3
+                    p-2.5 placeholder-gray-400
+                    text-white focus:border-blue-500'
                     placeholder='Search all product here'
                     required
                     value={productName} 
